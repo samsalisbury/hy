1f871aa9635687d4d76b3888a2f9502ac0685420
Sam Salisbury <samsalisbury@gmail.com>
2016-08-11 11:13:57 +0100

    Codec test: count marshal calls.

--- codec_test.go
+++ codec_test.go
@@ -2,8 +2,12 @@ package hy
 
 import (
 	"encoding/json"
+	"sync/atomic"
 	"testing"
 )
+
+func counter() *int64    { var c int64; return &c }
+func increment(c *int64) { atomic.AddInt64(c, 1) }
 
 func TestCodec_Write(t *testing.T) {
 	if testing.Short() {
@@ -11,8 +15,10 @@ func TestCodec_Write(t *testing.T) {
 	}
 	w := JSONWriter
 	w.RootDir = "testdata/out"
+	numCalls := counter()
 	w.MarshalFunc = func(v interface{}) ([]byte, error) {
-		return json.MarshalIndent(v, "  ", "  ")
+		increment(numCalls)
+		return json.MarshalIndent(v, "", "  ")
 	}
 	c := NewCodec(func(c *Codec) {
 		c.Writer = w
@@ -20,5 +26,10 @@ func TestCodec_Write(t *testing.T) {
 
 	if err := c.Write(testWriteStructData); err != nil {
 		t.Fatal(err)
+	}
+
+	expectedNumCalls := int64(19)
+	if *numCalls != expectedNumCalls {
+		t.Errorf("MarshalFunc called %d times; want %d", *numCalls, expectedNumCalls)
 	}
 }
