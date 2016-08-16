a6d6a6bd5c1d8d8385f63209716a8ee945bab158
Sam Salisbury <samsalisbury@gmail.com>
2016-08-16 11:22:37 +0100

    Add support for TextMarshaler in map key.

--- map.go
+++ map.go
@@ -1,6 +1,7 @@
 package hy
 
 import (
+	"encoding"
 	"fmt"
 	"reflect"
 
@@ -11,6 +12,10 @@ import (
 type MapNode struct {
 	*DirNodeBase
 	KeyType reflect.Type
+	// MarshalKey gets a string from the key.
+	MarshalKey func(reflect.Value) string
+	// UnmarshalKey sets a key from a string.
+	UnmarshalKey func(string, reflect.Value) error
 }
 
 // Detect returns nil if this base is a map.
@@ -29,15 +34,35 @@ func (MapNode) New(base NodeBase, c *Codec) (Node, error) {
 		},
 		KeyType: base.Type.Key(),
 	}
+	switch n.KeyType.Kind() {
+	case reflect.String:
+		n.MarshalKey = func(key reflect.Value) string {
+			return fmt.Sprint(key)
+		}
+		n.UnmarshalKey = func(s string, key reflect.Value) error {
+			key.Set(reflect.ValueOf(s))
+			return nil
+		}
+	}
+	if n.KeyType.Implements(tmType) {
+		n.MarshalKey = func(key reflect.Value) string {
+			tm := key.Interface().(encoding.TextMarshaler)
+			b, err := tm.MarshalText()
+			if err != nil {
+				panic(errors.Wrapf(err, "unmarshaling key %q for %s", key, n))
+			}
+			return string(b)
+		}
+	}
 	return n, errors.Wrap(n.AnalyseElemNode(n, c), "analysing map element node")
 }
 
+var tmType = reflect.TypeOf((*encoding.TextMarshaler)(nil)).Elem()
+var tuType = reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem()
+
 // ChildPathName returns the key as a string.
 func (n *MapNode) ChildPathName(child Node, key, val reflect.Value) string {
-	//if n.Field != nil && n.Field.KeyField != "" {
-	//	n.Field.GetKeyFunc.Call([]reflect.Value{val})
-	//}
-	return fmt.Sprint(key)
+	return n.MarshalKey(key)
 }
 
 // ReadTargets reads targets into map entries.
